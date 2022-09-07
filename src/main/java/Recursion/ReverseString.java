34d0a2e44132e1f767af9b62dd7fc31cdd2d3b34
nazliozcelik <nazliozelikk@icloud.com>
2022-09-07 17:03:02 +0300

    reverse a string

---
+++ src/main/java/Recursion/ReverseString.java
@@ -0,0 +1,33 @@
+package Recursion;
+
+public class ReverseString {
+
+    public static StringBuilder reverseStrings(String word) {
+        StringBuilder reversedString = new StringBuilder("");
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversedString.append(word.charAt(i));
+        }
+        return reversedString;
+    }
+
+    //Using reverse() function
+    public static StringBuilder withReverseFunc(String word){
+        return new StringBuilder(word).reverse();
+    }
+
+    //Recursive Solution
+    public static String recursiveReverse(String word){
+        if (word.length()==0 || word == null){
+            return "";
+        }
+        return recursiveReverse(word.substring(1)) + word.charAt(0);
+    }
+
+
+
+    public static void main(String[] args) {
+        System.out.println(reverseStrings("test"));
+        System.out.println(withReverseFunc("test"));
+        System.out.println(recursiveReverse("test"));
+    }
+}
